a65c93b97daefb2f5cf116cf388b641ac363f1dd
Jhonatan Alarcón <jhonatan.alarcon.ariza@gmail.com>
2021-03-16 08:29:15 -0500

    add static.json and fix router config

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static("build"));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(PORT, () => {
   console.log("Server started on port 5000");
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import './styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +9,7 @@ import reportWebVitals from './reportWebVitals';
 //console.log('RenderISIS3710')
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router basename={process.env.PUBLIC_URL}>< App /></Router>,
   document.getElementById('root')
 );
 
